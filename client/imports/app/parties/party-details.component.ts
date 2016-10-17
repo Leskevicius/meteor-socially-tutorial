e191fe866a910a5d2203ddb33fb4ffaebbc73ab8
leskevicius <rokaslopas@gmail.com>
2016-10-17 18:07:21 -0400

    moving to another computer

--- client/imports/app/parties/party-details.component.ts
+++ client/imports/app/parties/party-details.component.ts
@@ -1,4 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Subscription } from 'rxjs/Subscription';
+
+import 'rxjs/add/operator/map';
+
+import { Parties } from '../../../../both/collections/parties.collection';
+import { Party } from '../../../../both/models/party.model';
 
 import template from './party-details.component.html';
 
@@ -6,4 +13,36 @@ import template from './party-details.component.html';
   selector: 'party-details',
   template
 })
-export class PartyDetailsComponent {}
+export class PartyDetailsComponent implements OnInit, OnDestroy {
+  partyId: string;
+  paramsSub: Subscription;
+  party: Party;
+
+  constructor(
+    private route: ActivatedRoute
+  ) {}
+
+  ngOnInit() {
+    this.paramsSub = this.route.params
+      .map(params => params['partyId'])
+      .subscribe(partyId => {
+        this.partyId = partyId
+
+        this.party = Parties.findOne(this.partyId);
+      });
+  }
+
+  saveParty() {
+    Parties.update(this.party._id, {
+      $set: {
+        name: this.party.name,
+        description: this.party.description,
+        location: this.party.location
+      }
+    });
+  }
+
+  ngOnDestroy() {
+    this.paramsSub.unsubscribe();
+  }
+}

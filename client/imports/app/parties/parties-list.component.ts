a1ce81646b5dae7a5fb96327f10aecfce93fefcf
Rokas Leskevicius <rokas.l@ufl.edu>
2016-10-18 14:21:44 -0400

    .

--- client/imports/app/parties/parties-list.component.ts
+++ client/imports/app/parties/parties-list.component.ts
@@ -1,23 +1,58 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Observable } from 'rxjs/Observable';
+import { Subscription } from 'rxjs/Subscription';
+import { MeteorObservable } from 'meteor-rxjs';
 
 import { Parties } from '../../../../both/collections/parties.collection';
 import { Party } from '../../../../both/models/party.model';
 
 import template from './parties-list.component.html';
 
+interface Pagination {
+  limit: number;
+  skip: number;
+}
+
+interface Options extends Pagination {
+  [key: string]: any
+}
+
 @Component({
   selector: 'parties-list',
   template
 })
-export class PartiesListComponent {
+export class PartiesListComponent implements OnInit, OnDestroy {
   parties: Observable<Party[]>;
+  partiesSub: Subscription;
+  pageSize: number = 10;
+  curPage: number = 1;
+  nameOrder: number = 1;
 
-  constructor() {
-    this.parties = Parties.find({}).zone();
+  ngOnInit() {
+    const options: Options = {
+      limit: this.pageSize,
+      skip: (this.curPage - 1) * this.pageSize,
+      sort: { name: this.nameOrder }
+    };
+
+    this.partiesSub = MeteorObservable.subscribe('parties', options).subscribe(() => {
+      this.parties = Parties.find({}, {
+        sort: {
+          name: this.nameOrder
+        }
+      }).zone();
+    });
   }
 
   removeParty(party: Party): void {
     Parties.remove(party._id);
+  }
+
+  search(value: string): void {
+    this.parties = Parties.find(value ? { location: value } : {}).zone();
+  }
+
+  ngOnDestroy() {
+    this.partiesSub.unsubscribe();
   }
 }
--- client/imports/app/parties/party-details.component.ts
+++ client/imports/app/parties/party-details.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, OnDestroy } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Subscription } from 'rxjs/Subscription';
 import { Meteor } from 'meteor/meteor';
+import { MeteorObservable } from 'meteor-rxjs';
 
 import 'rxjs/add/operator/map';
 
@@ -18,6 +19,7 @@ export class PartyDetailsComponent implements OnInit, OnDestroy {
   partyId: string;
   paramsSub: Subscription;
   party: Party;
+  partySub: Subscription;
 
   constructor(
     private route: ActivatedRoute
@@ -29,7 +31,13 @@ export class PartyDetailsComponent implements OnInit, OnDestroy {
       .subscribe(partyId => {
         this.partyId = partyId
 
-        this.party = Parties.findOne(this.partyId);
+        if (this.partySub) {
+          this.partySub.unsubscribe();
+        }
+
+        this.partySub = MeteorObservable.subscribe('party', this.partyId).subscribe(() => {
+          this.party = Parties.findOne(this.partyId);
+        });
       });
   }
 
@@ -50,5 +58,6 @@ export class PartyDetailsComponent implements OnInit, OnDestroy {
 
   ngOnDestroy() {
     this.paramsSub.unsubscribe();
+    this.partySub.unsubscribe();
   }
 }
